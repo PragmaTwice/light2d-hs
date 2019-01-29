3cd67bc7e784fc3ef23ca52d223805af8677456a
PragmaTwice <e.d.boy@aliyun.com>
2019-01-29 10:30:10 +0800

    Test: dump all image && CI: ls dumped dir

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,16 @@
+import Light2D.Basic
+import Light2D.CSG
+import Light2D.Shapes
+import Light2D.Reflection
+import Light2D.Refraction
+import Debug.Trace
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = foldl (>>) (putStrLn "Start dumping images...") $ 
+    map (uncurry $ trace . (++ " part...")) [
+        ("Basic",       dumpBasicFile), 
+        ("CSG",         dumpCSGFile), 
+        ("Shapes",      dumpShapesFile), 
+        ("Reflection",  dumpReflectionFile), 
+        ("Refraction",  dumpRefractionFile)
+    ]
